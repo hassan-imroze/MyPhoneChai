0f6fc6058d7754add493d4b59a5329fc40037a42
Foyzul Karim <foyzulkarim@gmail.com>
2016-03-11 16:03:39 +0600

    phone service added

---
+++ Source/Phonechai/Phonechai.Service/PhoneService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Phonechai.Model;
+using Phonechai.Repository;
+using Phonechai.ViewModel;
+
+namespace Phonechai.Service
+{
+    public class PhoneService
+    {
+        public List<PhoneViewModel> GetAll()
+        {
+            PhoneRepository repository=new PhoneRepository(new BusinessDbContext());
+
+            IQueryable<Phone> queryable = repository.GetAll();
+            var phoneViewModels = queryable.ToList().Select(x => new PhoneViewModel(x)).ToList();
+            return phoneViewModels;
+        }
+    }
+}
--- Source/Phonechai/Phonechai.ViewModel/PhoneViewModel.cs
+++ Source/Phonechai/Phonechai.ViewModel/PhoneViewModel.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Phonechai.Model;
 
 namespace Phonechai.ViewModel
 {
     public class PhoneViewModel
     {
+        public PhoneViewModel(Phone phone)
+        {
+            Id = phone.Id;
+            Name = phone.Name;
+        }
+
         public string Id { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
